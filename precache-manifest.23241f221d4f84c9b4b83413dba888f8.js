02d722d746fe6519355489a865fef2a6c7ecc7d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-02 12:50:36 +0000

    Deploy Digitopia/sons to github.com/Digitopia/sons.git:gh-pages

---
+++ precache-manifest.23241f221d4f84c9b4b83413dba888f8.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e2cd90a94956bf15c8c",
+    "url": "/sons/css/app.66a98450.css"
+  },
+  {
+    "revision": "6e2cd90a94956bf15c8c",
+    "url": "/sons/js/app.a58a0d47.js"
+  },
+  {
+    "revision": "cfb6022fc0d8b69cacb7",
+    "url": "/sons/js/chunk-vendors.700039b9.js"
+  },
+  {
+    "revision": "ade3049495ed8773d99abdc8bd43c9cb",
+    "url": "/sons/index.html"
+  },
+  {
+    "revision": "852f8e32f0815e5e37b9a5071320e422",
+    "url": "/sons/admin/config.yml"
+  },
+  {
+    "revision": "a61d286e02b4244ae67f785719e2646d",
+    "url": "/sons/admin/index.html"
+  },
+  {
+    "revision": "aeb798620d226b8a405f33770e516d63",
+    "url": "/sons/audios/escola-5.json"
+  },
+  {
+    "revision": "a4c259fe901ab4acfbda87822abdccf5",
+    "url": "/sons/audios/escola1.json"
+  },
+  {
+    "revision": "cf5057db19d2751c1cc233f68fe2a61f",
+    "url": "/sons/audios/escola2.json"
+  },
+  {
+    "revision": "1bd8ad698adcefb07594b8c537df052d",
+    "url": "/sons/audios/escola3.json"
+  },
+  {
+    "revision": "8027908354fe5f7d50cc894953d8f680",
+    "url": "/sons/audios/escola4.json"
+  },
+  {
+    "revision": "f1ffcdb46f8c710bb235ab623a2fd99a",
+    "url": "/sons/audios/uploads/caramila.jpg"
+  },
+  {
+    "revision": "098998a90eb06e29f1c44a6a2f2fd203",
+    "url": "/sons/audios/uploads/e1.jpeg"
+  },
+  {
+    "revision": "2466ecd3c0d071091021226ec2b1bf2a",
+    "url": "/sons/audios/uploads/e2.jpeg"
+  },
+  {
+    "revision": "077189ee2a7484fa4f39a5ab2e22e851",
+    "url": "/sons/audios/uploads/e5.jpeg"
+  },
+  {
+    "revision": "88c6576b1178d243b19f5ca88e8bc37c",
+    "url": "/sons/audios/uploads/e3.jpeg"
+  },
+  {
+    "revision": "077189ee2a7484fa4f39a5ab2e22e851",
+    "url": "/sons/audios/uploads/e4.jpeg"
+  },
+  {
+    "revision": "36e12097785d0f536f3345b3290f99d9",
+    "url": "/sons/audios/uploads/joaodedeus.jpg"
+  },
+  {
+    "revision": "5ac38368931171c1e82720c260784e77",
+    "url": "/sons/audios/uploads/g1.wav"
+  },
+  {
+    "revision": "6ca6d5d612a9c3fa0894a43253732484",
+    "url": "/sons/audios/uploads/g2.wav"
+  },
+  {
+    "revision": "b0c093159b36d80ae1950c5d8c1c59dd",
+    "url": "/sons/_redirects"
+  },
+  {
+    "revision": "e6e62a3a4ca2df57c4d337d48e495803",
+    "url": "/sons/404.html"
+  },
+  {
+    "revision": "aa672e6a81b1db63b1bf29559ad48717",
+    "url": "/sons/audios/index.html"
+  },
+  {
+    "revision": "619d415578bac5f532b55bde32501e1c",
+    "url": "/sons/banks/aula/icons/clap.svg"
+  },
+  {
+    "revision": "40946e5f5818dd241ce7d24c06ab7999",
+    "url": "/sons/banks/aula/icons/kick.svg"
+  },
+  {
+    "revision": "45187f1f0859cc8ad7c3711d41cbc16b",
+    "url": "/sons/banks/aula/icons/snap.svg"
+  },
+  {
+    "revision": "31e9e903d0c7e31e68bf17adb9deaa26",
+    "url": "/sons/banks/aula/sounds/clap.mp3"
+  },
+  {
+    "revision": "30be526205a72ab3d5c7840b6a94b996",
+    "url": "/sons/banks/aula/sounds/kick.mp3"
+  },
+  {
+    "revision": "c06447f79cc5021217916121557b0145",
+    "url": "/sons/banks/orff/icons/flute.svg"
+  },
+  {
+    "revision": "3f32163e170c635c2a6d747ad2376b34",
+    "url": "/sons/banks/aula/sounds/snap.mp3"
+  },
+  {
+    "revision": "6c2aac6ea0be4c72cbd64e1ff31ab171",
+    "url": "/sons/banks/orff/icons/xylophone.svg"
+  },
+  {
+    "revision": "81ed8109c3ced2fd1ea8c3c9c7da77e3",
+    "url": "/sons/banks/orff/icons/marimba.png"
+  },
+  {
+    "revision": "bcdacfc61c031c7aa7da55b72dcf43a9",
+    "url": "/sons/banks/orff/sounds/marimba.wav"
+  },
+  {
+    "revision": "14550f0ef2c3691def50113e35610933",
+    "url": "/sons/banks/orff/sounds/xylophone.wav"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/sons/robots.txt"
+  },
+  {
+    "revision": "88454c49eb583430bb7eb38997a288cb",
+    "url": "/sons/banks/orff/sounds/recorder.wav"
+  },
+  {
+    "revision": "4292c38f7d1f28450de2b75cac4b7111",
+    "url": "/sons/banks/orff/sounds/glockenspiel.wav"
+  },
+  {
+    "revision": "17a59e9a496366ce462e57ce11556d37",
+    "url": "/sons/Bravura.otf"
+  },
+  {
+    "revision": "f51ccd8f1f090b9d48f7665c3541bd13",
+    "url": "/sons/audios/uploads/g4.wav"
+  },
+  {
+    "revision": "f51ccd8f1f090b9d48f7665c3541bd13",
+    "url": "/sons/audios/uploads/g5.wav"
+  },
+  {
+    "revision": "f51ccd8f1f090b9d48f7665c3541bd13",
+    "url": "/sons/audios/uploads/g3.wav"
+  },
+  {
+    "revision": "cf71ddd2fc1934c4f7bf94a334c58b1d",
+    "url": "/sons/audios/uploads/caramila.mp3"
+  },
+  {
+    "revision": "0ac989826fdaf444211026c2659516c2",
+    "url": "/sons/audios/uploads/joao-de-deus.mp3"
+  }
+];
